ae303cdbe7f43640003201652cb2c33d2d7872c9
Renae Heinrich <renae.heinrich@c02wx3xejg5j.myob.myobcorp.net>
2019-03-18 16:04:20 +1100

    blanked out test 3

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,13 +3,13 @@ public class Game {
     // score starts at 0
     private int score = 0;
 
-    // score based on pins knocked down but also if strike or spare happens in same turn
+    // roll calculates score, but name does not imply that
     public void roll(int pins){
         score += pins;
 
     }
 
-
+    // score does not calculate score, but name implies that it does
     public int score(){
         return score;
     }
