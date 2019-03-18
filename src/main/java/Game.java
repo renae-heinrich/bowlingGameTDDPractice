71b926b29d2a8e2262b5edafdc179b51ea0cf31a
Renae Heinrich <renae.heinrich@c02wx3xejg5j.myob.myobcorp.net>
2019-03-19 09:55:51 +1100

    refactoting Game class so roll method uses current roll to index the turn and pin value, score adds the roll index values up

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,16 +1,22 @@
 public class Game {
 
-    // score starts at 0
-    private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
 
-    // roll calculates score, but name does not imply that
+
     public void roll(int pins){
-        score += pins;
+        rolls[currentRoll++] = pins;
 
     }
 
-    // score does not calculate score, but name implies that it does
-    public int score(){
+    public int score() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++) {
+            score += rolls[i];
+        }
         return score;
     }
+
+
+
 }
